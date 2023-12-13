8f291eff442b8dd978c7d1ee630472e132a31c35
B8angho <bhjeon98@gmail.com>
2023-12-13 18:13:58 +0900

    update version1.0

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ProjectItem({ data }) {
   const title = data.properties.Name.title[0].plain_text;
   const github = data.properties.Github.url;
-  const youtube = data.properties.Youtube.url;
+  const demo = data.properties.Demo.url;
   const description = data.properties.Description.rich_text[0].plain_text;
   const imgSrc = data.cover.file?.url || data.cover.external.url;
   const tags = data.properties.Tags.multi_select;
@@ -48,13 +48,16 @@ export default function ProjectItem({ data }) {
         <h1 className="text-2xl font-bold">{title}</h1>
         <h3 className="mt-4 text-xl">{description}</h3>
         <a href={github}>깃허브 바로가기</a>
-        <a href={youtube}>유튜브 시연영상 보러가기</a>
+        <a href={demo}>데모페이지 바로가기</a>
         <p className="my-1 ">
           작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
         </p>
-        <div className="flex items-start mt-2">
+        <div className="flex flex-wrap items-start mt-2">
           {tags.map((aTag) => (
-            <h1 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30" key={aTag.id}>
+            <h1
+              className="px-2 py-1 mr-2 mb-2 rounded-md bg-green-200 dark:bg-green-600 w-30"
+              key={aTag.id}
+            >
               {aTag.name}
             </h1>
           ))}
--- pages/projects.js
+++ pages/projects.js
@@ -11,12 +11,12 @@ export default function Projects({ projects }) {
       <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
         <Head>
           <title>포트폴리오</title>
-          <meta name="description" content="good!" />
+          <meta name="description" content="good" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <h1 className="text-4xl font-bold sm:text-6xl">
+        {/* <h1 className="text-4xl font-bold sm:text-6xl">
           총 프로젝트 :<span className="pl-4 text-blue-500">{projects.results.length}</span>
-        </h1>
+        </h1> */}
 
         <div className="grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-2">
           {projects.results.map((aProject) => (
@@ -28,6 +28,10 @@ export default function Projects({ projects }) {
   );
 }
 
+// 빌드 타임에 호출
+// export async function getStaticProps() {
+
+// 각 요청 때마다 호출
 export async function getServerSideProps() {
   const options = {
     method: "POST",
@@ -47,7 +51,7 @@ export async function getServerSideProps() {
       page_size: 100,
     }),
   };
-
+  console.log(TOKEN);
   const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options);
 
   const projects = await res.json();
@@ -59,6 +63,8 @@ export async function getServerSideProps() {
   console.log(`projectNames : ${projectNames}`);
 
   return {
-    props: { projects }, // 페이지 컴포넌트로 전달되는 props
+    props: { projects }, // will be passed to the page component as props
+    // getStaticProps() 메소드를 사용한다면 revalidate 로 데이터 변경시 갱신가능!
+    // revalidate: 1 // 데이터 변경이 있으면 갱신 1초 마다
   };
 }
